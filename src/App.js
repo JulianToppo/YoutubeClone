d4518f907cd76d6810708b43d022a6bc637d3726
JulianToppo <juliantoppo95@gmail.com>
2024-01-25 13:17:09 +0530

    Addition of sidebar that appears on top of the header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
-    <div>
+    <div className='relative'>
      <Header/>
+     <Sidebar/>
     </div>
   );
 }
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,21 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Sidebar = () => {
-    const [firstChange, setfirstChange] = useState(0)
-    const [secondChange, setSecondChange] = useState(0)
-
-    const onClickHandler=(e)=>{
-        e.preventDefault();
-
-        setfirstChange(1);
-        setSecondChange(2);
-    }
-
   return (
-    <div>
-      <button onClick={onClickHandler}>Click Me</button>
+    <div className="flex flex-col absolute min-h-screen border-cyan-300 w-1/5 bg-slate-400  text-red-200 shadow-md z-50">
+         <div className='flex bg-white opacity-45 pt-1'>
+            <img className='h-10 mt-2' src='https://cdn4.iconfinder.com/data/icons/action-states-vol-3-glyph/48/Sed-10-512.png'></img>
+            <img className='h-16 -mt-1' src='https://cdn.mos.cms.futurecdn.net/8gzcr6RpGStvZFA2qRt4v6-1200-80.jpg' alt='youtubelogo'></img>
+        </div>
+      <div className="mt-2">
+        home
+      
+      </div>
+      <div>Shorts</div>
+      <div>Subscriptions</div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
